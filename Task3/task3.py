4123ee605ece787e9a3935653f1018cfce000736
dawsboss <gdawson2018@gmail.com>
2021-04-24 13:24:03 -0400

    small update to task4

---
+++ Task3/task3.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+def main():
+    lines = sys.stdin.read().split("\n")
+    lines = lines if lines[-1] != '' else lines[:-1]
+    arr = []
+    for i in range(1, len(lines)):
+        arr.append(lines[i])
+    rtn = arr[1:] + arr[:1]
+    print(rtn)
+
+
+
+if __name__ == '__main__':
+    print("Hello")
+    main()
