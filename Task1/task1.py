c7ba37355a41aafdb830b0086e388b527227142f
Ryan Rosiak <ryan11291129@gmail.com>
2021-04-24 09:21:44 -0400

    Finished task 1

--- Task1/task1.py
+++ Task1/task1.py
@@ -1,7 +1,37 @@
 import os
+import sys
+
+def fact(num):
+    total = 1
+    for counter in range(1, num + 1):
+        total *= counter
+
+    return total
+
+def counter_zeros(num):
+    counter_zero = 0
+
+    while num > 0:
+        temp =  num % 10
+        if temp == 0:
+            counter_zero += 1
+        num /= 10
+
+    return counter_zero
 
 def main():
-    pass
+    lines = sys.stdin.read().split("\n")
+
+    lines = lines if lines[-1] != '' else lines[:-1]
+
+    for i in range(0, len(lines)):
+        num = int(lines[i])
+
+        factorial = fact(num)
+        zeros = counter_zeros(factorial)
+
+        print(f'{zeros}')
+
 
 if __name__ == '__main__':
     main()
