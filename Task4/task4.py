317c47b3468191923e5317b6c41b9a5611a98c35
dawsboss <gdawson2018@gmail.com>
2021-04-24 10:25:04 -0400

    Task4 done, needs checking

--- Task4/task4.py
+++ Task4/task4.py
@@ -5,10 +5,8 @@ def main():
     lines = sys.stdin.read().split("\n")
     lines = lines if lines[-1] != '' else lines[:-1]
     arr = []
-    print("Test")
     for i in range(1, len(lines)):
         arr.append(lines[i])
-    print("ree")
     rtn = arr[1:] + arr[:1]
     print(rtn)
 
